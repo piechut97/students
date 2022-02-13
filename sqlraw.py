e917a559b66fd0d370ee29551fe8086332c503e7
piechut97 <47992364+piechut97@users.noreply.github.com>
2022-02-13 17:05:17 +0100

    Add files via upload
    Finish project

--- sqlraw.py
+++ sqlraw.py
@@ -1,8 +1,6 @@
-#! /usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 import sqlite3
-from czytanie_danych import pobierz_dane
+from pobieranie_danych import pobierz_dane
 
 # utworzenie połączenia z bazą przechowywaną na dysku
 # lub w pamięci (':memory:')
@@ -85,17 +83,17 @@ cur.executemany(
 
 
 
-print("1. Wypisanie rekordów znajdujących się w bazie danych")
-print("2. Dodanie rekordu do bazy danych ")
-print("3. Zmodyfikowanie rekordów w bazie danych")
-print("4. Usunięcie rekordu z bazy")
-print("5. Koniec programu")
-
 while True:
+    print("1. Wypisanie rekordów znajdujących się w bazie danych")
+    print("2. Dodanie rekordu do bazy danych ")
+    print("3. Zmodyfikowanie rekordów w bazie danych")
+    print("4. Usunięcie rekordu z bazy")
+    print("5. Koniec programu")
     menu = str(input("Podaj cyfrę z menu, aby wybrać co chcesz zrobić: "))
 
     if menu == "1":
         czytajdane()
+
     elif menu == "2":
         rekord = input("Po przecinku podaj dane do dodania do bazy ")
         print()
@@ -103,8 +101,37 @@ while True:
         print(rekord)
         print()
         cur.execute('INSERT INTO uczen VALUES(NULL,?,?,?)', rekord)
+
     elif menu == "3":
-        print("nie mam pomyslu")
+        while True:
+            print("Menu modyfikacji rekordu:")
+            print("1. Zmiana imienia")
+            print("2. Zmiana nazwiska")
+            print("3. Zmiana klasy")
+            print("4. Wyjście z menu modyfikacji rekordu")
+
+            modd = str(input("Co chcesz zrobić? "))
+
+            if modd == "1":
+                new_name = str(input("Podaj nowe imie "))
+                index = int(input("Podaj numer rekordu ucznia do zmiany imienia: "))
+                cur.execute('UPDATE uczen SET imie=? WHERE id=?', (new_name, index,))
+
+            elif modd == "2":
+                new_name = str(input("Podaj nowe nazwisko "))
+                index = int(input("Podaj numer rekordu ucznia do zmiany nazwiska: "))
+                cur.execute('UPDATE uczen SET nazwisko=? WHERE id=?', (new_name, index,))
+
+            elif modd == "3":
+                new_name = str(input("Podaj nowa klase "))
+                index = int(input("Podaj numer rekordu ucznia do zmiany klasy: "))
+                cur.execute("SELECT id FROM klasa WHERE nazwa=?", (new_name,))
+                id_klasy = cur.fetchone()[0]
+                cur.execute('UPDATE uczen SET klasa_id=? WHERE id=?', (id_klasy, index,))
+
+            elif modd == "4":
+                break
+            break
     elif menu == "4":
         indeks = input("Podaj indeks ucznia do usuniecia: ")
         cur.execute('DELETE FROM uczen WHERE id=?', (indeks))
@@ -114,3 +141,4 @@ while True:
         exit()
     else:
         print("Błąd danych!! Podaj poprawne dane!")
+
